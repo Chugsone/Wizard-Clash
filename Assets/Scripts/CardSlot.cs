ed2981fd6e5e8bca15f47dc01df8020e8f1f2e69
MUKILTEO\1725525 <deklinfis@outlook.com>
2025-11-20 11:18:37 -0800

    code stuff

---
+++ Assets/Scripts/CardSlot.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class CardSlot : MonoBehaviour
+{
+    private void OnMouseEnter()
+    {
+        
+    }
+
+}
--- Assets/Scripts/DragDrop.cs
+++ Assets/Scripts/DragDrop.cs
@@ -5,23 +5,33 @@ using UnityEngine.EventSystems;
 
 public class DragDrop : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
-    Transform parentAfterDrag;
+    public static DragDrop currentDraggedItem;
+
+    public Transform parentAfterDrag;
+    private Vector2 originalPosition;
+  
 
     public void OnBeginDrag(PointerEventData eventData)
     {
         Debug.Log("OnBeginDrag");
+        originalPosition = this.transform.position;
         parentAfterDrag = this.transform.parent;
         this.transform.SetParent(this.transform.root);
         transform.SetAsLastSibling();
+
+        currentDraggedItem = this;
     }
     public void OnDrag(PointerEventData eventData)
     {
         Debug.Log("OnDrag");
+        
         transform.position = Input.mousePosition;
     }
     public void OnEndDrag(PointerEventData eventData)
     {
         Debug.Log("OnEndDrag");
+        transform.position = originalPosition;
         this.transform.SetParent(parentAfterDrag);
+        currentDraggedItem = null;
     }
 }
