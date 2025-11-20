b0cc67dcbac0b04a1210e52e4900d052ae1ef51f
MUKILTEO\1725525 <deklinfis@outlook.com>
2025-11-20 11:32:32 -0800

    stuff

--- Assets/Scripts/CardSlot.cs
+++ Assets/Scripts/CardSlot.cs
@@ -4,7 +4,11 @@ public class CardSlot : MonoBehaviour
 {
     private void OnMouseEnter()
     {
-        
+        DragDrop.currentDraggedItem.parentAfterDrag = this.transform;
     }
 
+    private void OnMouseExit()
+    {
+        DragDrop.currentDraggedItem.parentAfterDrag = null;
+    }
 }
