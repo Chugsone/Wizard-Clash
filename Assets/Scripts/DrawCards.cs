0e6b4cc5718b23154f02280547e8674415d2d3dd
MUKILTEO\1726377 <chasebannavong@gmail.com>
2025-11-20 12:24:54 -0800

    ;g

--- Assets/Scripts/DrawCards.cs
+++ Assets/Scripts/DrawCards.cs
@@ -1,8 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class DrawCards : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public GameObject Card1;
+    public GameObject Card2;
+    public GameObject Card3;
+    public GameObject PlayerArea;
+
+    public void OnClick()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            GameObject card = Instantiate(Card1, new Vector2(0,0), Quaternion.identity);
+            card.transform.SetParent(PlayerArea.transform, false);
+        }
+    }
+
+
     void Start()
     {
         
