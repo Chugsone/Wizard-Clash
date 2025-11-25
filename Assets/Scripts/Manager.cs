3ed2a69fc9c26115db912c1fc03cac29fc2de817
MUKILTEO\1726377 <chasebannavong@gmail.com>
2025-11-25 13:38:48 -0800

    ergtyhjkl

--- Assets/Scripts/Manager.cs
+++ Assets/Scripts/Manager.cs
@@ -5,63 +5,75 @@ using UnityEngine.UI;
 
 public class Manager : MonoBehaviour
 {
-    
+    public enum Choice
+    {
+        Fire,
+        Water,
+        Earth
+    }
+
     public Text resultText;
     public Image AIChoice;
 
-    public string[] Choices;
+    public Choice[] Choices;
     public Sprite Fire, Water, Earth;
 
-    public void Play(string myChoice)
+    public void PlayString(string choice)
     {
-        string randomChoice = Choices[Random.Range(0, Choices.Length)];
+        if (System.Enum.TryParse<Choice>(choice, out Choice choice2))
+            Play(choice2);
+    }
+
+    public void Play(Choice myChoice)
+    {
+        Choice randomChoice = Choices[Random.Range(0, Choices.Length)];
 
         switch (randomChoice)
         {
-            case "Earth":
+            case Choice.Earth:
                 switch (myChoice)
                 {
-                    case "Earth":
+                    case Choice.Earth:
                         resultText.text = "Draw!";
                         break;
 
-                    case "Fire":
+                    case Choice.Fire:
                         resultText.text = "You Win!";
                         break;
 
-                    case "Water":
+                    case Choice.Water:
                         resultText.text = "You Lose!";
                         break;
                 }
                 AIChoice.sprite = Earth;
                 break;
 
-            case "Water":
+            case Choice.Water:
                 switch (myChoice)
                 {
-                    case "Earth":
+                    case Choice.Earth:
                         resultText.text = "You Lose!";
                         break;
-                    case "Fire":
+                    case Choice.Fire:
                         resultText.text = "You Win!";
                         break;
-                    case "Water":
+                    case Choice.Water:
                         resultText.text = "Draw!";
                         break;
                 }
                 AIChoice.sprite = Water;
                 break;
 
-            case "Fire":
+            case Choice.Fire:
                 switch (myChoice)
                 {
-                    case "Earth":
+                    case Choice.Earth:
                         resultText.text = "You Win!";
                         break;
-                    case "Fire":
+                    case Choice.Fire:
                         resultText.text = "Draw!";
                         break;
-                    case "Water":
+                    case Choice.Water:
                         resultText.text = "You Lose!";
                         break;
                 }
