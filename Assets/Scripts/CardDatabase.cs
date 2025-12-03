3e6fa1fbb1f7484f22f00b2949c2d16453e3739d
MUKILTEO\1726377 <chasebannavong@gmail.com>
2025-12-03 12:28:41 -0800

    e

--- Assets/Scripts/CardDatabase.cs
+++ Assets/Scripts/CardDatabase.cs
@@ -8,6 +8,9 @@ public class CardDatabase : MonoBehaviour
 
     void Awake()
     {
-        //cardList.Add(new Card(0, "Flood", 0, 0, "Beats Fire", Resources.Load<Sprite>("1")));
+        cardList.Add(new Card(0, "Flood", "Beats Fire", Resources.Load<Sprite>("1")));
+        cardList.Add(new Card(1, "Fire", "Beats Wind", Resources.Load<Sprite>("2")));
+        cardList.Add(new Card(2, "Wind", "Beats Earth", Resources.Load<Sprite>("3")));
+        cardList.Add(new Card(3, "Earth", "Beats Lightning", Resources.Load<Sprite>("4")));
     }
 }
--- Assets/Scripts/PlayerDeck.cs
+++ Assets/Scripts/PlayerDeck.cs
@@ -1,11 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerDeck : MonoBehaviour
 {
+    public List<Card> deck = new List<Card>();
+
+    public int x;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        x = 0;
+
+        for(int i=0;i<40;i++)
+        {
+            x = Random.Range(1, 3);
+            deck[i]= CardDatabase.cardList[x];
+        }
     }
 
     // Update is called once per frame
