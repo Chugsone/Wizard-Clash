6879d776ed19e72ba0d148c376e5c3a9c42d8432
MUKILTEO\1726377 <chasebannavong@gmail.com>
2025-12-05 11:57:32 -0800

    Reset some

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -1,33 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
-[System.Serializable]
 
 public class Card : MonoBehaviour
 {
-    public int id;
-    public string cardName;
-    public string description;
-
-    public Sprite thisImage;
-    
-    public Card()
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
     {
-        cardName = "Default Card";
-        description = "This is a default card description.";
+        
     }
 
-    public Card(int Id, string CardName, string Description)
+    // Update is called once per frame
+    void Update()
     {
-        id = Id;
-        this.cardName = CardName;
-        this.description = Description;
-
-        thisImage = thisImage;
-    }
-
-    public Card(int Id, string CardName, string Description, Sprite sprite) : this(Id, CardName, Description)
-    {
+        
     }
 }
