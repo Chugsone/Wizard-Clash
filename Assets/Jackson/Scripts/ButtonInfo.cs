99e04c931e057a12f83c7109b76dd6bdbc972a0b
MUKILTEO\1601139 <jaxiscool17@gmail.com>
2025-12-15 13:38:15 -0800

    Shop Done pretty much

--- Assets/Jackson/Scripts/ButtonInfo.cs
+++ Assets/Jackson/Scripts/ButtonInfo.cs
@@ -1,16 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ButtonInfo : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+
+    public int ItemID;
+    public TMP_Text PriceTxt;
+    public TMP_Text QuantutyTxt;
+    public GameObject ShopManager;
 
     // Update is called once per frame
     void Update()
     {
-        
+        PriceTxt.text = "Price $" + ShopManager.GetComponent<ShopManagerScript>().shopItems[2, ItemID].ToString();
+        QuantutyTxt.text = ShopManager.GetComponent<ShopManagerScript>().shopItems[3, ItemID].ToString();
     }
 }
